80266b00301669a8198102a1b1e6916cb41edd28
Sergey Dolin <v-sedoli@micorosoft.com>
2020-08-12 13:53:29 +0500

    Remove ActiveFeatures layer on dismount

--- src/react/components/Ol/ActiveFeatures.tsx
+++ src/react/components/Ol/ActiveFeatures.tsx
@@ -4,24 +4,22 @@ import VectorSource from 'ol/source/Vector';
 import useVisibleFeatures from './hooks/useVisibleFeatures';
 import log from '../../../log';
 import olMapContext from './context/map';
-import {Features} from '../../../app-rx/catalog';
+import OlFeature from 'ol/Feature';
 
 const source = new VectorSource({wrapX: false});
 const layer = new VectorLayer({source: source});
 
 const ActiveFeatures: React.FunctionComponent = (): React.ReactElement => {
-  const features: Features = useVisibleFeatures();
+  const features: OlFeature[] = useVisibleFeatures();
   const map = React.useContext(olMapContext);
 
   useEffect(() => {
     map.addLayer(layer);
+    return () => map.removeLayer(layer)
   }, [map]);
 
-  useEffect(() => {
-    source.clear();
-    source.addFeatures(features);
-  }, [features, map]);
-
+  source.clear();
+  source.addFeatures(features);
 
   log.render('ActiveFeatures');
 

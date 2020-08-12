254a8756549c2fef1dbc2371962bf74200dfa058
Sergey Dolin <v-sedoli@micorosoft.com>
2020-08-12 13:55:42 +0500

    reuse visible observable

--- src/react/components/Ol/hooks/useVisibleFeatures.ts
+++ src/react/components/Ol/hooks/useVisibleFeatures.ts
@@ -4,9 +4,23 @@ import {VisibleFeatures} from '../../../../app-rx/ui/designer';
 import OlFeature from 'ol/Feature';
 import useObservable from '../../../hooks/useObservable';
 import {map} from 'rxjs/operators';
+import {Feature} from '../../../../app-rx/catalog';
+import {Observable} from 'rxjs';
 
-const transformVisibleFeatures = (features: VisibleFeatures): OlFeature => Array.from(features).map(feature => olFeatureFactory(feature));
-const visibleFeatures = () => transformVisibleFeatures(getDesigner().visibleFeatures);
+const transformVisibleFeatures = (features: VisibleFeatures): OlFeature[] => Array.from<Feature>(features).map(feature => olFeatureFactory(feature));
 
-const useVisibleFeatures = () => useObservable(getDesigner().visibleFeatures.observable().pipe(map(transformVisibleFeatures)), Array.from(visibleFeatures()));
+const observable: Observable<OlFeature[]> = getDesigner()
+  .visibleFeatures
+  .observable()
+  .pipe(
+    map(
+      (f) => {
+        return transformVisibleFeatures(f)
+      }))
+// const visibleFeatures = () => transformVisibleFeatures(getDesigner().visibleFeatures);
+
+const useVisibleFeatures = (): OlFeature[] => {
+  const visibleFeatures: OlFeature[] = transformVisibleFeatures(getDesigner().visibleFeatures)
+  return useObservable(observable, visibleFeatures);
+}
 export default useVisibleFeatures;

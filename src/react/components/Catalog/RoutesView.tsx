47f62761f8d5b784bc6471862c0f77988877bf91
Sergey Dolin <v-sedoli@micorosoft.com>
2020-08-16 20:13:23 +0500

    Fix RoutesView not updated

--- src/react/components/Catalog/RoutesView.tsx
+++ src/react/components/Catalog/RoutesView.tsx
@@ -8,6 +8,8 @@ import RouteView from './RouteView';
 import {Category} from '../../../app-rx/catalog';
 import RouteEdit from './RouteEdit';
 import ConfirmDialog from '../Confirm';
+import T from '../../../l10n';
+import {map} from 'rxjs/operators';
 
 const getClassName = (isDraggingOver: boolean): string => `list${isDraggingOver ? ' dragging-over' : ''}`;
 
@@ -35,11 +37,15 @@ const wording = getWording();
 const RoutesView: React.FunctionComponent<{ category: Category; }> = ({category}): React.ReactElement => {
   log.render('Routes');
 
-  const routes = useObservable(category.routes.observable(), category.routes);
+  const routes = useObservable(category.routes.observable()
+    .pipe(
+      map(r => Array.from(r))
+    ),
+    Array.from(category.routes));
   const routeEdit = useObservable(catalogUI.routeEditObservable(), catalogUI.routeEdit);
   const routeDelete = useObservable(catalogUI.routeDeleteObservable(), catalogUI.routeDelete);
   const handleDragEnd = useCallback(
-    ({source: {index: indexS}, destination: {index: indexD}}): void => routes.reorder(indexS, indexD),
+    ({source: {index: indexS}, destination: {index: indexD}}): void => category.routes.reorder(indexS, indexD),
     []);
   const handleAdd = useCallback(() => {
     category.routes.add(null).then(route => catalogUI.startEditRoute(route))
@@ -52,7 +58,7 @@ const RoutesView: React.FunctionComponent<{ category: Category; }> = ({category}
           className={getClassName(snapshotDroppable.isDraggingOver)}
           ref={providedDroppable.innerRef}
         >
-          {Array.from(routes).map((item, index): React.ReactElement =>
+          {routes.map((item, index): React.ReactElement =>
             <Draggable draggableId={item.id} index={index} key={item.id} >
               {(provided, snapshot): React.ReactElement => <div
                 className={'draggable folder-level-1'}
@@ -73,7 +79,7 @@ const RoutesView: React.FunctionComponent<{ category: Category; }> = ({category}
       </Droppable >
     </DragDropContext >
     <button className="add" onClick={handleAdd} type="button" >
-      Add
+      {T`Add`}
     </button >
     {routeEdit && <RouteEdit route={routeEdit} />}
     {routeDelete && <ConfirmDialog

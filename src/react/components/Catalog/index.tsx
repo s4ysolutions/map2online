6e362781b711d1d5b78b81ab831117a5fe2ca345
Sergey Dolin <v-sedoli@micorosoft.com>
2020-08-20 13:22:39 +0500

    Do not render Catalog if panel closed

--- src/react/components/Catalog/index.tsx
+++ src/react/components/Catalog/index.tsx
@@ -2,20 +2,22 @@ import './styles.scss';
 import * as React from 'react';
 import CatalogNavigation from './CatalogNavigation';
 import FeaturesView from './FeaturesView';
-import {getCatalogUI} from '../../../di-default';
+import {getCatalogUI, getWorkspace} from '../../../di-default';
 import useObservable from '../../hooks/useObservable';
 import log from '../../../log';
 import CategoriesView from './CategoriesView';
 import RoutesView from './RoutesView';
 
 const catalog = getCatalogUI();
+const workspace = getWorkspace();
 
 const Catalog: React.FunctionComponent<{}> = (): React.ReactElement => {
+  const openCatalog = useObservable(workspace.catalogObservable(), workspace.catalogOpen)
   const selectedCategory = useObservable(catalog.selectedCategoryObservable(), catalog.selectedCategory);
   const selectedRoute = useObservable(catalog.selectedRouteObservable(), catalog.selectedRoute);
-  log.render(`Catalog category=${selectedCategory} route=${selectedRoute}`);
+  log.render(`Catalog open=${openCatalog} category=${selectedCategory} route=${selectedRoute}`);
 
-  return <div className="catalog" >
+  return openCatalog && <div className="catalog" >
     {selectedCategory && selectedRoute
       ? <FeaturesView route={selectedRoute} />
       : selectedCategory

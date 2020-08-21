ddcd45c68cc0dec5fd0d6f4618031c0887448aa2
Sergey Dolin <v-sedoli@micorosoft.com>
2020-08-21 23:38:00 +0500

    fix kml imported and add more debug

--- src/importer/default/kml-parser.ts
+++ src/importer/default/kml-parser.ts
@@ -36,9 +36,9 @@ const parseCoordinates = (text: string): Coordinate[] =>
 
 export const parseKMLString = (file: File, kml: string): Promise<ImportedFolder[]> => {
   const parser = sax.parser(true, {normalize: true, trim: true, xmlns: true});
-  const rootFolders = [] as ImportedFolder[];
-  const foldersStack = [] as ImportedFolder[];
-  const parseStateStack = [ParseState.NONE] as ParseState[];
+  const rootFolders: ImportedFolder[] = [];
+  const foldersStack: ImportedFolder[] = [];
+  const parseStateStack: ParseState[] = [ParseState.NONE];
   const lastState = () => parseStateStack[parseStateStack.length - 1];
   const lastFolder = () => foldersStack.length === 0 ? null : foldersStack[foldersStack.length - 1];
   let currentFeature = null as FeatureProps;
@@ -46,10 +46,12 @@ export const parseKMLString = (file: File, kml: string): Promise<ImportedFolder[
   return new Promise<ImportedFolder[]>((rs, rj) => {
     parser.onopentag = (node) => {
       const name = node.name.toUpperCase();
+      log.debug('opentag', name);
       switch (name) {
         case 'DOCUMENT':
         case 'FOLDER': {
           parseStateStack.push(ParseState.FOLDER);
+          log.debug('parseStateStack.push(ParseState.FOLDER)', parseStateStack);
           const nextFolder: ImportedFolder = {
             features: [],
             folders: [],
@@ -59,8 +61,10 @@ export const parseKMLString = (file: File, kml: string): Promise<ImportedFolder[
             parent: lastFolder(),
           };
           if (lastFolder()) {
+            // there's a
             lastFolder().folders.push(nextFolder);
           } else {
+            // root folder named by file name
             nextFolder.name = file.name;
             rootFolders.push(nextFolder);
           }
@@ -69,6 +73,7 @@ export const parseKMLString = (file: File, kml: string): Promise<ImportedFolder[
         }
         case 'PLACEMARK': {
           parseStateStack.push(ParseState.FEATURE);
+          log.debug('parseStateStack.push(ParseState.FEATURE)', parseStateStack);
           currentFeature = {
             color: Color.RED,
             description: '',
@@ -84,69 +89,110 @@ export const parseKMLString = (file: File, kml: string): Promise<ImportedFolder[
         case 'NAME':
           if (lastState() === ParseState.FOLDER) {
             parseStateStack.push(ParseState.FOLDER_NAME);
+            log.debug('parseStateStack.push(ParseState.FOLDER_NAME)', parseStateStack);
           } else if (lastState() === ParseState.FEATURE) {
             parseStateStack.push(ParseState.FEATURE_NAME);
+            log.debug('parseStateStack.push(ParseState.FEATURE_NAME)', parseStateStack);
           }
           break;
         case 'DESCRIPTION':
           if (lastState() === ParseState.FOLDER) {
             parseStateStack.push(ParseState.FOLDER_DESCRIPTION);
+            log.debug('parseStateStack.push(ParseState.FOLDER_DESCRIPTION)', parseStateStack);
           } else if (lastState() === ParseState.FEATURE) {
             parseStateStack.push(ParseState.FEATURE_DESCRIPTION);
+            log.debug('parseStateStack.push(ParseState.FEATURE_DESCRIPTION)', parseStateStack);
           }
           break;
         case 'COORDINATES':
           parseStateStack.push(ParseState.COORDINATES);
+          log.debug('parseStateStack.push(ParseState.COORDINATES)', parseStateStack);
           break;
         default:
           parseStateStack.push(ParseState.UNKNOWN);
+          log.debug('parseStateStack.push(ParseState.UNKNOWN)', parseStateStack, name);
       }
     };
     parser.onclosetag = (nodeName) => {
       const name = nodeName.toUpperCase();
+      log.debug('closetag', name);
       switch (name) {
         case 'DOCUMENT':
         case 'FOLDER': {
+          log.debug('parseStateStack.pop(ParseState.FOLDER)', parseStateStack);
           const state = parseStateStack.pop();
           if (state === ParseState.FOLDER) {
             if (foldersStack.length === 0) {
               log.warn('No folders on the stack');
             }
+            log.debug('parseStateStack.pop(ParseState.FOLDER) 2', parseStateStack);
             foldersStack.pop();
           } else {
-            log.warn('Folder or Document close tag does not match');
+            log.warn(`${nodeName} tag does not match`, state);
+            return rj(Error(`${nodeName} tag does not match, lasts state=${lastState()}`));
           }
           break;
         }
-        case 'DESCRIPTION':
         case 'NAME':
           if (lastState() === ParseState.FOLDER_NAME || lastState() === ParseState.FEATURE_NAME) {
+            log.debug('parseStateStack.pop(ParseState.FOLDER_NAME || FEATUTE_NAME)', parseStateStack);
             parseStateStack.pop();
+          } else {
+            log.warn(`${nodeName} tag does not match`, lastState());
+            return rj(Error(`${nodeName} tag does not match, lasts state=${lastState()}`));
+          }
+          break;
+        case 'DESCRIPTION':
+          if (lastState() === ParseState.FOLDER_DESCRIPTION || lastState() === ParseState.FEATURE_DESCRIPTION) {
+            log.debug('parseStateStack.pop(ParseState.FOLDER_DESCRIPTION || FEATUTE_DESCRIPTION)', parseStateStack);
+            parseStateStack.pop();
+          } else {
+            log.warn(`${nodeName} tag does not match`, lastState());
+            return rj(Error(`${nodeName} tag does not match, lasts state=${lastState()}`));
           }
           break;
         case 'COORDINATES':
+          if (lastState() === ParseState.COORDINATES) {
+            log.debug('parseStateStack.pop(ParseState.COORDINATES)', parseStateStack);
+            parseStateStack.pop();
+          } else {
+            log.warn(`${nodeName} tag does not match`, lastState());
+            return rj(Error(`${nodeName} tag does not match, lasts state=${lastState()}`));
+          }
+          break;
         case 'PLACEMARK':
+          if (lastState() === ParseState.FEATURE) {
+            log.debug('parseStateStack.pop(ParseState.FEATURE)', parseStateStack);
+            parseStateStack.pop();
+          } else {
+            log.warn(`${nodeName} tag does not match`, lastState());
+            return rj(Error(`${nodeName} tag does not match, lasts state=${lastState()}`));
+          }
+          break;
         default:
-          parseStateStack.pop();
+          if (lastState() === ParseState.UNKNOWN) {
+            log.debug('parseStateStack.pop(ParseState.UNKNOWN)', parseStateStack);
+            parseStateStack.pop();
+          } else {
+            log.warn(`${nodeName} tag does not match`, lastState());
+            return rj(Error(`${nodeName} tag does not match, lasts state=${lastState()}`));
+          }
       }
     };
     parser.ontext = (text) => {
+      log.debug('text', text);
       switch (lastState()) {
         case ParseState.FOLDER_NAME:
           lastFolder().name = text.trim();
-          parseStateStack.pop();
           break;
         case ParseState.FEATURE_NAME:
           currentFeature.title = text.trim();
-          parseStateStack.pop();
           break;
         case ParseState.FOLDER_DESCRIPTION:
           lastFolder().description = text.trim();
-          parseStateStack.pop();
           break;
         case ParseState.FEATURE_DESCRIPTION:
           currentFeature.description = text.trim();
-          parseStateStack.pop();
           break;
         case ParseState.COORDINATES: {
           const coordinates = parseCoordinates(text.trim());
@@ -159,18 +205,22 @@ export const parseKMLString = (file: File, kml: string): Promise<ImportedFolder[
               coordinates,
             };
           }
-          parseStateStack.pop();
           break;
         }
       }
     };
     parser.onend = () => {
+      log.debug('end');
+      log.debug('resolve');
       rs(rootFolders);
     };
     parser.onerror = (e) => {
+      log.debug('error');
+      log.debug('reject');
       rj(e);
     };
     parser.write(kml);
+    log.debug('close');
     parser.close();
   });
 };
